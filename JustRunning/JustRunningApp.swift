379644916cb6feb6704713cac71b2c6ff7c1a276
AuTtx <1932063838@qq.com>
2023-04-28 14:46:07 +0800

    Initial Commit

---
+++ JustRunning/JustRunningApp.swift
@@ -0,0 +1,20 @@
+//
+//  JustRunningApp.swift
+//  JustRunning
+//
+//  Created by 许智尧 on 2023/4/28.
+//
+
+import SwiftUI
+
+@main
+struct JustRunningApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
